55c6c86956a989da168a1155a7c5676a332dfe26
Ashutosh Gautam <ashutoshgautam9628@outlook.com>
2020-11-30 11:57:24 +0530

    Add files via upload
    Hackerank Anagram Solution

--- string_anagram.java
+++ string_anagram.java
@@ -1,34 +1,69 @@
 package competitive;
 
-import java.util.*;
+import java.util.Scanner;
+
 public class string_anagram {
-    public static void main(String[] args) {
-        String a = "silent";
-        String b = "listen";
 
-        boolean isanagram = false;
-        boolean visted[]=new boolean[b.length()];
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        int temp,tempp, size1,size;
+        a=a.toUpperCase();
+        b=b.toUpperCase();
 
-        for (int i = 0; i < a.length(); i++) {
-            char ch = a.charAt(i);
-            isanagram = false;
-            for (int j = 0; j < b.length(); j++) {
-                if (ch == b.charAt(j)&&!visted[j]) {
-                    visted[j]=true;
-                    isanagram = true;
+        char charArray[] = a.toCharArray();
+        char charArrayy[] = b.toCharArray();
+
+        size = charArray.length;
+        size1 = charArrayy.length;
+
+        for(int i = 0; i < size; i++ ) {
+            for(int j = i+1; j < size; j++) {
+                if(charArray[i]>charArray[j]) {
+                    temp = charArray[i];
+
+                    charArray[i] = charArray[j];
+
+                    charArray[j] = (char) temp;
+
+
+
                 }
             }
+        }
+        for(int i = 0; i < size1; i++ ) {
+            for(int j = i+1; j < size1; j++) {
+                if(charArrayy[i]>charArrayy[j]) {
 
-            if (!isanagram) {
-                break;
+                    tempp = charArrayy[i];
+                    charArrayy[i] = charArrayy[j];
+                    charArrayy[j] = (char) tempp;
+
+
+                }
             }
         }
-        if(isanagram)
+        String str1=String.valueOf(charArray);
+        String str2=String.valueOf(charArrayy);
+
+        System.out.println(String.valueOf(charArray));
+        System.out.println(str2);
+
+        if(str1.equalsIgnoreCase(str2)&&(a.length()==b.length()))
         {
-            System.out.println("Anagram");
+            return true;
         }
         else {
-            System.out.println("Not Anagram");
+            return false;
         }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
     }
 }
